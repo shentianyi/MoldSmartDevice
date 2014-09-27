9d05cad5011d8d26bef3fa3d12c0f9cf13b254d2
Charlot <7wangsong@gmail.com>
2014-09-28 00:07:30 +0800

    time

--- MoldMgnDesktop/ToolingManWPF/Main.xaml.cs
+++ MoldMgnDesktop/ToolingManWPF/Main.xaml.cs
@@ -30,7 +30,7 @@ namespace ToolingManWPF
             InitializeComponent();
             dTimer.IsEnabled = true;
 
-            dTimer.Interval = new TimeSpan(0, 0, 10);
+            dTimer.Interval = new TimeSpan(0, 30, 0);
             dTimer.Tick += new EventHandler(DataTimer_Tick);
             dTimer.Start();
         }
